e18cb64e12e5b02103a1f9a73a462715d70388a6
c4Lnn <1186678828@qq.com>
2021-05-31 14:28:03 +0800

    add new file

---
+++ 蓝桥杯/PREV/41.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+#define SZ(x) (int)(x).size()
+#define ALL(x) (x).begin(),(x).end()
+#define PB push_back
+#define MP make_pair
+#define FI first
+#define SE second
+using namespace std;
+typedef double DB;
+typedef long double LD;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<PII> VPII;
+// head
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int x;cin>>x;
+    VI res;
+    do {
+        --x;
+        res.PB(x%26);
+        x/=26;
+    } while(x);
+    for(int i=SZ(res)-1;~i;i--) cout<<(char)(res[i]+'A');
+    cout<<'\n';
+    return 0;
+}
