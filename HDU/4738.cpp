7d49f1ef3d07b0d2a4d4a4201475e874d5088400
c4Lnn <1186678828@qq.com>
2021-11-20 02:06:59 +0800

    update

--- HDU/4738.cpp
+++ HDU/4738.cpp
@@ -1,65 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define SZ(x) (int)(x).size()
 #define ALL(x) (x).begin(),(x).end()
 #define PB push_back
+#define EB emplace_back
 #define MP make_pair
 #define FI first
 #define SE second
 using namespace std;
 typedef double DB;
+typedef long double LD;
 typedef long long LL;
 typedef unsigned long long ULL;
 typedef pair<int,int> PII;
 typedef vector<int> VI;
+typedef vector<LL> VLL;
 typedef vector<PII> VPII;
-//head
+// head
 const int N=1e3+5;
-int n,m,cnt,dfn[N],low[N];
+VI g[N];
 VPII e;
-VI b,G[N];
-void addEdge(int u,int v,int w) {
-    e.PB(MP(v,w));
-    e.PB(MP(u,w));
-    G[u].PB(SZ(e)-2);
-    G[v].PB(SZ(e)-1);
+int n,m,cnt,dfn[N],low[N],fa[N],res;
+void init() {
+    res=0x3f3f3f3f;
+    cnt=0;
+    e.clear();
+    for(int i=1;i<=n;i++) g[i].clear(),dfn[i]=low[i]=0;
+}
+void add_edge(int u,int v,int w) {
+    e.EB(v,w);
+    e.EB(u,w);
+    g[u].PB(SZ(e)-2);
+    g[v].PB(SZ(e)-1);
 }
 void tarjan(int u,int id) {
     dfn[u]=low[u]=++cnt;
-    for(auto x:G[u]) {
-        int v=e[x].FI,w=e[x].SE;
+    for(auto x:g[u]) {
+        int v=e[x].FI;
         if(!dfn[v]) {
+            fa[v]=u;
             tarjan(v,x);
             low[u]=min(low[u],low[v]);
-            if(dfn[u]<low[v]) b.PB(w);
+            if(dfn[u]<low[v]) res=min(res,e[x].SE);
         }
         else if(id!=(x^1)) low[u]=min(low[u],dfn[v]);
     }
 }
-void getBridge() {
-    b.clear();
-    e.clear();
-    cnt=0;
-    for(int i=1;i<=n;i++) dfn[i]=low[i]=0;
-    int t=0;
-    for(int i=1;i<=n;i++) if(!dfn[i]) t++,tarjan(i,-1);
-    int res=0x3f3f3f3f;
-    for(auto x:b) res=min(res,x);
-    if(t>1) puts("0");
-    else if(res==0x3f3f3f3f) puts("-1");
-    else if(res==0) puts("1");
-    else printf("%d\n",res);
-}
 int main() {
-    //freopen("D:/Program Files (x86)/Sublime Text 3/in.txt","r",stdin);
-    while(~scanf("%d%d",&n,&m)) {
-        if(n==0&&m==0) break;
-        for(int i=1;i<=n;i++) G[i].clear();
-        for(int i=1;i<=m;i++) {
-            int u,v,w;
-            scanf("%d%d%d",&u,&v,&w);
-            addEdge(u,v,w);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    while(cin>>n>>m) {
+        if(!n&&!m) break;
+        init();
+        for(int i=0;i<m;i++) {
+            int u,v,w;cin>>u>>v>>w;
+            add_edge(u,v,w);
         }
-        getBridge();
+        int t=0;
+        for(int i=1;i<=n;i++) if(!dfn[i]) tarjan(i,-1),++t;
+        if(t!=1) cout<<0<<'\n';
+        else {
+            if(!res) ++res;
+            cout<<(res==0x3f3f3f3f?-1:res)<<'\n';
+        }
     }
     return 0;
 }
