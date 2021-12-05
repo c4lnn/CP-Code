abd73589516f404f747c58bef30cd9137d218d5f
c4Lnn <1186678828@qq.com>
2021-12-06 01:31:08 +0800

    update

--- 蓝桥杯/BASIC/02.cpp
+++ 蓝桥杯/BASIC/02.cpp
@@ -1,32 +1,8 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <vector>
-#define SZ(x) (int)(x).size()
-#define ALL(x) (x).begin(),(x).end()
-#define PB push_back
-#define MP make_pair
-#define FI first
-#define SE second
+#include <bits/stdc++.h>
 using namespace std;
-typedef double DB;
-typedef long double LD;
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int,int> PII;
-typedef vector<int> VI;
-typedef vector<PII> VPII;
-// head
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    LL n;cin>>n;
-    cout<<(n*n+n)/2<<'\n';
+    long long n;
+    cin >> n;
+    cout << (n * n + n) / 2 << '\n';
     return 0;
 }
--- 蓝桥杯/BASIC/03.cpp
+++ 蓝桥杯/BASIC/03.cpp
@@ -1,30 +1,9 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <vector>
-#define SZ(x) (int)(x).size()
-#define ALL(x) (x).begin(),(x).end()
-#define PB push_back
-#define MP make_pair
-#define FI first
-#define SE second
+#include <bits/stdc++.h>
 using namespace std;
-typedef double DB;
-typedef long double LD;
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int,int> PII;
-typedef vector<int> VI;
-typedef vector<PII> VPII;
-// head
+const double PI = acos(-1);
 int main() {
-    int r;scanf("%d",&r);
-    printf("%.7f\n",atan(1.0)*4*r*r);
+    int r;
+    cin >> r;
+    cout << fixed << setprecision(7) << PI * r * r << '\n';
     return 0;
 }
