16a113431d8d7b05cadb418a2c92651aaff89642
c4Lnn <1186678828@qq.com>
2021-12-11 23:04:01 +0800

    update

--- 蓝桥杯/BASIC/2.cpp
+++ 蓝桥杯/BASIC/2.cpp
@@ -1,36 +1,11 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <vector>
-#define SZ(x) (int)(x).size()
-#define ALL(x) (x).begin(),(x).end()
-#define PB push_back
-#define MP make_pair
-#define FI first
-#define SE second
+#include <bits/stdc++.h>
 using namespace std;
-typedef double DB;
-typedef long double LD;
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int,int> PII;
-typedef vector<int> VI;
-typedef vector<PII> VPII;
-// head
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    for(int i=0;i<32;i++) {
-        for(int j=4;~j;j--) {
-            cout<<((i>>j)&1);
+    for (int i = 0; i < 32; i++) {
+        for (int j = 4; ~j; j--) {
+            cout << ((i >> j) & 1);
         }
-        cout<<'\n';
+        cout << '\n';
     }
     return 0;
 }
--- 蓝桥杯/BASIC/4.cpp
+++ 蓝桥杯/BASIC/4.cpp
@@ -1,41 +1,16 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <vector>
-#define SZ(x) (int)(x).size()
-#define ALL(x) (x).begin(),(x).end()
-#define PB push_back
-#define MP make_pair
-#define FI first
-#define SE second
+#include <bits/stdc++.h>
 using namespace std;
-typedef double DB;
-typedef long double LD;
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int,int> PII;
-typedef vector<int> VI;
-typedef vector<PII> VPII;
-// head
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n;cin>>n;
-    int sum=0,mn=0x3f3f3f3f,mx=0xc0c0c0c0;
-    for(int i=1;i<=n;i++) {
-        int x;cin>>x;
-        sum+=x;
-        mn=min(mn,x);
-        mx=max(mx,x);
+    int n;
+    cin >> n;
+    int mx = 0xc0c0c0c0, mn = 0x3f3f3f3f, sum = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        mx = max(mx, x);
+        mn = min(mn, x);
+        sum += x;
     }
-    cout<<mx<<'\n';
-    cout<<mn<<'\n';
-    cout<<sum<<'\n';
+    cout << mx << '\n' << mn << '\n' << sum << '\n';
     return 0;
 }
